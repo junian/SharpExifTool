69d951f5121ef97281573744a4fd7e58cc6ae9a4
Junian Triajianto <author@junian.dev>
2024-05-10 14:44:50 +0700

    Refactor

--- src/SharpExifTool/ExifTool.cs
+++ src/SharpExifTool/ExifTool.cs
@@ -2,40 +2,40 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Juniansoft.SharpExifTool
 {
     internal class ExifTool: IDisposable
     {
-        const string c_exeName = "exiftool.exe";
-        const string c_arguments = @"-stay_open 1 -@ - -common_args -charset UTF8 -G1 -args";
-        const string c_exitCommand = "-stay_open\n0\n-execute\n";
-        const int c_timeout = 30000;    // in milliseconds
-        const int c_exitTimeout = 15000;
+        const string ExifToolBin = "exiftool.exe";
+        const string Arguments = @"-stay_open 1 -@ - -common_args -charset UTF8 -G1 -args";
+        readonly string ExitCommand = $"-stay_open{Environment.NewLine}0{Environment.NewLine}-execute{Environment.NewLine}";
+        const int Timeout = 30000;    // in milliseconds
+        const int ExitTimeout = 15000;
 
-        static Encoding s_Utf8NoBOM = new UTF8Encoding(false);
+        private readonly Encoding Utf8NoBOM = new UTF8Encoding(false);
 
-        Process m_exifTool;
-        StreamWriter m_in;
-        StreamReader m_out;
+        private Process _processExifTool;
+        private StreamWriter _writer;
+        private StreamReader _reader;
 
         public ExifTool()
         {
             // Prepare process start
-            var psi = new ProcessStartInfo(c_exeName, c_arguments);
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
-            psi.RedirectStandardInput = true;
-            psi.RedirectStandardOutput = true;
-            psi.StandardOutputEncoding = s_Utf8NoBOM;
+            var psi = new ProcessStartInfo(ExifToolBin, Arguments)
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                StandardOutputEncoding = Utf8NoBOM
+            };
 
             try
             {
-                m_exifTool = Process.Start(psi);
-                if (m_exifTool == null || m_exifTool.HasExited)
+                _processExifTool = Process.Start(psi);
+                if (_processExifTool == null || _processExifTool.HasExited)
                 {
                     throw new ApplicationException("Failed to launch ExifTool!");
                 }
@@ -47,22 +47,22 @@ namespace Juniansoft.SharpExifTool
 
             // ProcessStartInfo in .NET Framework doesn't have a StandardInputEncoding property (though it does in .NET Core)
             // So, we have to wrap it this way.
-            m_in = new StreamWriter(m_exifTool.StandardInput.BaseStream, s_Utf8NoBOM);
-            m_out = m_exifTool.StandardOutput;
+            _writer = new StreamWriter(_processExifTool.StandardInput.BaseStream, Utf8NoBOM);
+            _reader = _processExifTool.StandardOutput;
         }
 
         public void GetProperties(string filename, ICollection<KeyValuePair<string, string>> propsRead)
         {
-            m_in.Write(filename);
-            m_in.Write("\n-execute\n");
-            m_in.Flush();
+            _writer.Write(filename);
+            _writer.Write($"{Environment.NewLine}-execute{Environment.NewLine}");
+            _writer.Flush();
 #if EXIF_TRACE
             Debug.WriteLine(filename);
             Debug.WriteLine("-execute");
 #endif
             for (; ; )
             {
-                var line = m_out.ReadLine();
+                var line = _reader.ReadLine();
 #if EXIF_TRACE
                 Debug.WriteLine(line);
 #endif
@@ -84,7 +84,7 @@ namespace Juniansoft.SharpExifTool
 
         protected virtual void Dispose(bool disposing)
         {
-            if (m_exifTool != null)
+            if (_processExifTool != null)
             {
                 if (!disposing)
                 {
@@ -92,14 +92,14 @@ namespace Juniansoft.SharpExifTool
                 }
 
                 // If process is running, shut it down cleanly
-                if (!m_exifTool.HasExited)
+                if (!_processExifTool.HasExited)
                 {
-                    m_in.Write(c_exitCommand);
-                    m_in.Flush();
+                    _writer.Write(ExitCommand);
+                    _writer.Flush();
 
-                    if (!m_exifTool.WaitForExit(c_exitTimeout))
+                    if (!_processExifTool.WaitForExit(ExitTimeout))
                     {
-                        m_exifTool.Kill();
+                        _processExifTool.Kill();
                         Debug.Fail("Timed out waiting for exiftool to exit.");
                     }
 #if EXIF_TRACE
@@ -110,18 +110,18 @@ namespace Juniansoft.SharpExifTool
 #endif
                 }
 
-                if (m_out != null)
+                if (_reader != null)
                 {
-                    m_out.Dispose();
-                    m_out = null;
+                    _reader.Dispose();
+                    _reader = null;
                 }
-                if (m_in != null)
+                if (_writer != null)
                 {
-                    m_in.Dispose();
-                    m_in = null;
+                    _writer.Dispose();
+                    _writer = null;
                 }
-                m_exifTool.Dispose();
-                m_exifTool = null;
+                _processExifTool.Dispose();
+                _processExifTool = null;
             }
         }
 
