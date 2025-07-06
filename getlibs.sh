91f2c0bda0b6e75d115a25eb81af7e2355082980
Junian Triajianto <author@junian.dev>
2025-07-06 11:16:40 +0700

    Uses exiftool v13.32

--- getlibs.sh
+++ getlibs.sh
@@ -11,10 +11,11 @@ echo "Download finished."
 
 echo "Extracting files ..."
 
-mkdir -p libs/ExifTool.Win
+mkdir -p libs/
 unzip -o libs/exiftool-*.zip -d libs
 rm libs/exiftool-*.zip
-mv libs/exiftool*.exe libs/ExifTool.Win/exiftool.exe
+mv libs/exiftool* libs/ExifTool.Win
+mv libs/ExifTool.Win/exiftool*.exe libs/ExifTool.Win/exiftool.exe
 
 mkdir -p libs/
 tar -jxvf libs/Image-ExifTool*.tar.gz -C libs/
@@ -22,4 +23,3 @@ rm libs/Image-ExifTool*.tar.gz
 mv libs/Image-ExifTool* libs/ExifTool.Unix
 
 echo "Files extraction finished. You can use them for development now."
-
